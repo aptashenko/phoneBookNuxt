d4ac5b502d9394532fcde2d7a607ef690aab49ea
Artem Ptashenko <118530074+artem-fe01@users.noreply.github.com>
2023-02-12 11:13:30 +0200

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,9 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    publicPath: '/_nuxt/dist/'
+  },
 
   dayjs: {
     locales: ['en'],
